c7314bf858b21b289a4a903dc8648e39d3569ee0
Derek Daquel <30646661+ddaquel@users.noreply.github.com>
2022-12-01 22:10:47 +1100

    update (#2)

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,92 +1,98 @@
-import styles from './home.module.css'
-import Image from 'next/image'
-import { Grid, Stack } from '@mui/material'
-import Section from './components/Section/Section'
-import ProjectCard, { IProjectCardProps } from './components/ProjectCard/ProjectCard'
-import { NextPage } from 'next'
-import { useEffect, useState } from 'react'
+import styles from "./home.module.css";
+import Image from "next/image";
+import { Grid, Stack } from "@mui/material";
+import Section from "./components/Section/Section";
+import ProjectCard, {
+  IProjectCardProps,
+} from "./components/ProjectCard/ProjectCard";
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-
   const [loading, setLoading] = useState(true);
-  
+
   const projects: IProjectCardProps[] = [
-    {
-      title: "SPOONS",
-      description: "Online multiplayer based on the popular card game Spoons",
-      imgSrc: "/assets/home/Spoons.svg",
-      href: "https://drkdql.com/spoons"
-    }
-  ]
+    // {
+    //   title: "SPOONS",
+    //   description: "Online multiplayer based on the popular card game Spoons",
+    //   imgSrc: "/assets/home/Spoons.svg",
+    //   href: "https://spoons.drkdql.com/",
+    //   live: false,
+    // },
+  ];
 
-  const blogPosts = [
+  const blogPosts = [];
 
-  ]
-
-  const [instagramPosts, setInstagramPosts] = useState<number[]>([])
+  const [instagramPosts, setInstagramPosts] = useState<number[]>([]);
 
   useEffect(() => {
-    const posts:any[] = []
-    setInstagramPosts(posts)
-    setLoading(false)
-  }, [])
+    const posts: any[] = [];
+    setInstagramPosts(posts);
+    setLoading(false);
+  }, []);
 
- 
   return (
     <>
       {/* HEADER */}
       <header className={styles.header}>
-          <Stack alignItems={"center"} padding="10px">
-            <Image src="/assets/home/Logo.svg" height={70} width={1000} alt="DRKDQL personal brand logo"/>
-          </Stack>
+        <Stack alignItems={"center"} padding="10px">
+          <Image
+            src="/assets/home/Logo.svg"
+            height={70}
+            width={1000}
+            alt="DRKDQL personal brand logo"
+          />
+        </Stack>
       </header>
       {/* MAIN CONTENT */}
       <main className={styles.main}>
-        { loading && <Stack> Loading ... </Stack>}
-        { !loading && 
-          <Stack> 
+        {loading && <Stack> Loading ... </Stack>}
+        {!loading && (
+          <Stack>
             {/* SECTION - PROJECTS */}
-            <Section title="projects"> 
-              <Grid container> 
+            <Section title="projects">
+              Coming soon...
+              {/* <Grid container>
                 {projects.map((p, i) => {
                   return (
-                    <Grid item xs key={`project-${p.title}`}> 
-                      <ProjectCard title={p.title} description={p.description} imgSrc={p.imgSrc} href={p.href}/>
+                    <Grid item xs key={`project-${p.title}`}>
+                      <ProjectCard
+                        title={p.title}
+                        description={p.description}
+                        imgSrc={p.imgSrc}
+                        href={p.href}
+                        live={p.live}
+                      />
                     </Grid>
-                  )
+                  );
                 })}
-              </Grid>
+              </Grid> */}
             </Section>
 
             {/* SECTION - BLOG POSTS */}
-            <Section title="Blog Posts"> 
-              
-              Coming soon...
-            </Section>
-            
+            <Section title="Blog Posts">Coming soon...</Section>
+
             {/* SECTION - SOCIAL FEEDS */}
-            <Section title="Social Feeds"> 
-                <Stack>
-                   {/* INSTAGRAM */}
-                  <h3> Instagram </h3>
-                  Coming soon...
-                </Stack>
-                <br/>
-                <Stack>
-                  {/* TIKTOK */}
-                  <h3> TikTok </h3>
-                  Coming soon...
-                </Stack>
+            <Section title="Social Feeds">
+              <Stack>
+                {/* INSTAGRAM */}
+                <h3> Instagram </h3>
+                Coming soon...
+              </Stack>
+              <br />
+              <Stack>
+                {/* TIKTOK */}
+                <h3> TikTok </h3>
+                Coming soon...
+              </Stack>
             </Section>
           </Stack>
-        }
-        
+        )}
       </main>
       {/* FOOTER */}
-      <footer>
-      </footer>
+      <footer></footer>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
