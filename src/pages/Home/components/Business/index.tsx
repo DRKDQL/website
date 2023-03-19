c6a43679f6e45127e99b8586fd9fbc0648fd6455
ddaquel <derekdaquel@gmail.com>
2023-03-20 00:12:57 +1100

    1.0.3: Minor fix for project links

--- src/pages/Home/components/Business/index.tsx
+++ src/pages/Home/components/Business/index.tsx
@@ -18,7 +18,7 @@ const Business = (props: IBusinessProps) => {
       <ProjectGrid>
         <ProjectCard
           title="Speak & Shine Speech Pathology"
-          link="www.speakandshinespeechpathology.com.au"
+          link="speakandshinespeechpathology.com.au"
           caption="I built a website for my cousin's Mobile Speech pathology business that diagnoses and treats communication difficulties in children 1 - 18 years in the area of Marrickville and surrounding Suburbs."
           tools={[
             "NextJS",
