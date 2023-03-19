3766d986e2f2b2b021fae99a2aaf145cb7fad552
ddaquel <derekdaquel@gmail.com>
2023-03-19 23:30:00 +1100

    fix/cloudflare-setup: update packages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: false,
+  images: {
+    loader: "akamai",
+    path: "/",
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: "@svgr/webpack",
+    });
+    return config;
+  },
 };
